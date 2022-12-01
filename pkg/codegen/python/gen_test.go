32a37d5935e44b8d4bf898af9e61268545227c5e
Anthony King <anthony@datapane.com>
2022-12-01 02:25:57 +0000

    fix: propagate that we're building for a dictionary
    This lets us catch Sequences, mappings, etc..
    
    Also drop the redundant 'Required', they're required by default.

--- pkg/codegen/python/gen_test.go
+++ pkg/codegen/python/gen_test.go
@@ -242,7 +242,7 @@ func TestGenerateTypeNames(t *testing.T) {
 		require.True(t, ok)
 
 		return func(t schema.Type) string {
-			return root.typeString(t, false, false)
+			return root.typeString(t, false, false, false)
 		}
 	})
 }
